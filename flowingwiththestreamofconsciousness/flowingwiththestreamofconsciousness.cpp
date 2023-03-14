e03f3b0be03c9b277e09d23b078353f92d374c3c
llKatzll <bluedia0113@gmail.com>
2023-03-14 17:54:48 +0900

    ok

--- flowingwiththestreamofconsciousness/flowingwiththestreamofconsciousness.cpp
+++ flowingwiththestreamofconsciousness/flowingwiththestreamofconsciousness.cpp
@@ -1,26 +1,41 @@
 ﻿
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-class what {
-public:
-	void lessgo() {
-		cin >> n;
-		for (int i = 0; i <= n; a++) {
-			for (int b = 1; b <= n; b++) {
-				if (a + b > c && a + b + c == n) {
-					cout << a << " " << b << " " << c << endl;
-				}
-			}
-		}
-		if (cnt == 0) {
-			cout << "=1";
+void lol() {
+	int random[10];
+	int total = 0;
+	srand(100);
+	for (int i = 0; i < 10; i++) {
+		random[i] = rand();
+		cout << random[i] << " ";
+	}
+	for (int i = 0; i < 10; i++) {
+		if (random[i] > total) {
+			total = random[i];
 		}
 	}
-private:
-	int n, a, b, c;
-};
+	cout << endl;
+	cout << total << "이 제일 큼." << endl;
+}
+
+void fate() {
+	vector<int> score;
+	int num = 0;
+	int sum = 0;
+
+	while (true) {
+		cin >> num;
+		if (num == -1) break;
+		score.push_back(num);
+		sum += num;
+	}
+	cout << "평균은 :" << sum / score.size();
+}
+
 int main() {
-	
+	//lol();
+	fate();
 }
