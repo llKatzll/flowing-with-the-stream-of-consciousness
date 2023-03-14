9cd387146299f8782bbe4fc3eb13d973a957a54c
llKatzll <bluedia0113@gmail.com>
2023-03-15 00:03:00 +0900

    prac

--- flowingwiththestreamofconsciousness/flowingwiththestreamofconsciousness.cpp
+++ flowingwiththestreamofconsciousness/flowingwiththestreamofconsciousness.cpp
@@ -4,38 +4,18 @@
 
 using namespace std;
 
-void lol() {
-	int random[10];
-	int total = 0;
-	srand(100);
-	for (int i = 0; i < 10; i++) {
-		random[i] = rand();
-		cout << random[i] << " ";
-	}
-	for (int i = 0; i < 10; i++) {
-		if (random[i] > total) {
-			total = random[i];
-		}
-	}
-	cout << endl;
-	cout << total << "이 제일 큼." << endl;
-}
+// 재귀함수 예시
+int n;
 
-void fate() {
-	vector<int> score;
-	int num = 0;
-	int sum = 0;
-
-	while (true) {
-		cin >> num;
-		if (num == -1) break;
-		score.push_back(num);
-		sum += num;
+void f(int a) {
+	if (a > n) {
+		return;
 	}
-	cout << "평균은 :" << sum / score.size();
+	cout << (a) << endl;
+	f(a - 1);
 }
 
 int main() {
-	//lol();
-	fate();
+	cin >> n;
+	f(10);
 }
