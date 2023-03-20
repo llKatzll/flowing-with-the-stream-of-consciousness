852a46c090ff964ed8957aa33d8dbe569588e8b7
llKatzll <bluedia0113@gmail.com>
2023-03-20 23:15:45 +0900

    bruh2

--- flowingwiththestreamofconsciousness/flowingwiththestreamofconsciousness.cpp
+++ flowingwiththestreamofconsciousness/flowingwiththestreamofconsciousness.cpp
@@ -1,3 +1,26 @@
 ﻿#include <iostream>
 #include <vector>
-int main() {int timer;std::string a;std::cin >> timer;std::vector<std::string> h;h.resize(timer);for (int i = 0; i < timer; i++) {std::cin >> a;h[i] = a;}for (int x = 0; x < timer; x++) {std::cout << h[x][0];std::cout << h[x][h[x].length()-1] << std::endl;}}//흐하학 최강의 1줄 코드다 ㅋㅋㅋㅋㅋㅋ
+
+using namespace std;
+//int n;
+//int k;
+//
+//int solve(int oo, int xx, int y) {
+//	if (oo > n || y >= k) {
+//		return 0;
+//	}
+//	if (oo == n && xx == n) {
+//		return 1;
+//	}
+//	if (chk[oo][xx][y] == 1) {
+//		return memo[oo][xx][y];
+//		chk[oo][xx][y] = 1;
+//		return memo[oo][xx][y] = solve(oo + 1, xx + 1, y + 1) + solve(oo + 2, xx + 1, y + 1)
+//			+ solve(oo + 1, xx + 2, y + 1) + solve(oo + 2, xx + 2, y + 1);
+//	}
+//}
+
+
+int main() {
+	
+}
