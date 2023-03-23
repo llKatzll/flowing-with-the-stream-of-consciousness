3afc1bbdd8b9fb1b5da7649452cafee925412cc3
llKatzll <bluedia0113@gmail.com>
2023-03-23 23:13:49 +0900

    천!!사가 왔습니다 천사가 왔습니다
    세이탄!! 세이탄 윌 다이!! 세이탄 다이!!

--- flowingwiththestreamofconsciousness/flowingwiththestreamofconsciousness.cpp
+++ flowingwiththestreamofconsciousness/flowingwiththestreamofconsciousness.cpp
@@ -2,25 +2,28 @@
 #include <vector>
 
 using namespace std;
-int n, door1, door2, m;
-int arr[21];
-int answer = 9999;
-void solve(int d1, int d2, int dis, int cnt) {
-	if (m == cnt) {
-		answer = min(answer, dis);
-		return;
-	}
-	solve(arr[cnt], d2, arr[cnt] - d1, cnt + 1);
-	solve(d1, arr[cnt], arr[cnt] - d2, cnt + 1);
+//int n, door1, door2, m;
+//int arr[21];
+//int answer = 9999;
+//void solve(int d1, int d2, int dis, int cnt) {
+//	if (m == cnt) {
+//		answer = min(answer, dis);
+//		return;
+//	}
+//	solve(arr[cnt], d2, arr[cnt] - d1, cnt + 1);
+//	solve(d1, arr[cnt], arr[cnt] - d2, cnt + 1);
+//}
+//
+
+void satanwilldie() {
+	cout << "갓 뎀 할렐루야!" << endl;
+	cout << "천!!!!사가 왔습니다!!" << endl;
+	cout << "천사가 왔습니다!!" << endl;
+	cout << "세이탄!!" << endl;
+	cout << "세이탄 윌 다이!!" << endl;
+
 }
 
-
 int main() {
-	cin >> n;
-	cin >> door1 >> door2;
-	cin >> m;
-	for (int i = 0; i < mm; i++) {
-		cin >> arr[1];
-		//wotdafuc
-	}
+	satanwilldie();
 }
