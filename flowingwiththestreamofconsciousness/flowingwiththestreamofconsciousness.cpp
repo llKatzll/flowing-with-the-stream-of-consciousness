c5871fed5e3c8c720fcbfe9b68647609ea97ec91
llKatzll <bluedia0113@gmail.com>
2023-03-27 23:10:18 +0900

    clear

--- flowingwiththestreamofconsciousness/flowingwiththestreamofconsciousness.cpp
+++ flowingwiththestreamofconsciousness/flowingwiththestreamofconsciousness.cpp
@@ -45,50 +45,6 @@ using namespace std;
 
 
 int main() {
-	// 문자 비교
-	// string example = AA;
-	// example[0] == example[1] -> true;
-
-	// 문자 삭제하는거
-	// string example = ABCDEF;
-	// example.erase(1, 3); (위지, 길이)
-	// example -> AEF;
-
-	// 문장 길이
-	// string example = ABC;
-	// example.length() -> 3
-
-	string input;
-	string output;
-	cin >> input;
-
-	//------------------------
-	char storage = 0;
-	for (int i = 0; i < input.length(); i++) {
-		if (storage != input[i]) {
-			storage = input[i];
-		}
-		else if (storage == input[i]) {
-			input.erase(i, 1);
-			i--;
-		}
-		output = input;
-		//
-	}
-
-	//------------------------
-
-
-	cout << output << endl;
+	
 }
 
-
-/*
-A -> A
-ABCDEF -> ABCDEF
-AAAAA -> A
-AABCCDDEEFF -> ABCDEF
-ABBCCCDDDDEEEEEFFFFFF -> ABCDEF
-AABBAABB -> ABAB
-ABAABABA - > ABABA
-*/
