448b62426878810a122fcf91aff1e3a99fe354e3
llKatzll <bluedia0113@gmail.com>
2023-03-28 19:40:35 +0900

    prac

--- flowingwiththestreamofconsciousness/flowingwiththestreamofconsciousness.cpp
+++ flowingwiththestreamofconsciousness/flowingwiththestreamofconsciousness.cpp
@@ -2,49 +2,43 @@
 #include <vector>
 
 using namespace std;
-//int n, door1, door2, m;
-//int arr[21];
-//int answer = 9999;
-//void solve(int d1, int d2, int dis, int cnt) {
-//	if (m == cnt) {
-//		answer = min(answer, dis);
-//		return;
-//	}
-//	solve(arr[cnt], d2, arr[cnt] - d1, cnt + 1);
-//	solve(d1, arr[cnt], arr[cnt] - d2, cnt + 1);
-//}
-//
 
-//	int number;
-//	cin >> number;
-//	vector<int> st;
-//	vector<int> score;
-//	st.resize(number);
-//	score.resize(number);
-//
-//	cout << "for 1" << endl;
-//	for (int x = 0; x < number; x++) {
-//		cin >> score[x];
-//		st[x] = 1;
-//	}
-//
-//	cout << "for 2" << endl;
-//	for(int i = 0; i < number; i++) {
-//		//yoyoyo it's hump day
-//		for (int x = 0; x < number; x++) {
-//			if (score[i] < score[x]) {
-//				st[i] = st[i] + 1;
-//			}
-//		}
-//	}
-//
-//	for (int r = 0; r < 5; r++) {
-//		cout << st[r] << " " << endl;
-//	}
-//}
+class letscleanup {
+public:
+	void e() {
+		for (int i = 0; i < 5; i++) {
+			if (i == 0) {
+				cout << "입력" << endl;
+			}
+			else {
+				cout << "한 번 더" << endl;
+			}
+			cin >> anythingelse;
+			po[i] = anythingelse;
+		}
+
+		cout << "자, 이거 맞으시죠" << endl;
+		for (int x = 0; x < 5; x++) {
+			cout << po[x] << " , ";
+		}
+
+		for (int p = 0; p < 5; p++) {
+			s += po[p];
+		}
+		s = s / 5;
+		cout << " = ";
+		cout << s;
+	}
+private:
+	double anythingelse = 0;
+	double po[5] = {0,0,0,0,0};
+	double s = 0;
+};
+
 
 
 int main() {
-	
+	letscleanup clean;
+	clean.e();
 }
 
